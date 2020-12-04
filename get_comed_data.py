36aa84e66927458d1eedaefddc9137a834a4ef6b
wang1784 <57235185+wang1784@users.noreply.github.com>
2020-12-04 16:02:56 -0500

    get energy consumption for each account

--- get_comed_data.py
+++ get_comed_data.py
@@ -1,22 +1,41 @@
 import pandas as pd
 import numpy as np
 
+###use chicago_hourly function to get a dataframe of hourly consumption per account
+
 class get_comed_data():
     def __init__(self):
-        self.df = pd.read_csv('ComEdAccountsbyPremiseZipCode.csv', thousands = ',')
-        self.chicago = self.df[self.df['Name'] == 'Chicago, IL']
-    #
-    # def get_acct_int(self):
-    #     chicago_acct =
-    #     for eachacct in range(len(self.chicago['Accounts'])):
-    #         acct_int = eachacct.strip(',')
-    #         self.chicago.loc[each]
+        #read dataframe
+        self.zip = pd.read_csv('ComEdAccountsbyPremiseZipCode.csv', thousands = ',')
+        self.chicago = self.zip[self.zip['Name'] == 'Chicago, IL']
+        self.zip = self.zip.astype({'Accounts':int}) #make number of accounts integer
+        self.chicago = self.chicago.astype({'Accounts': int}) #make number of accounts integer
+        self.comed = pd.read_csv('COMED_hourly.csv')
 
     def find_chicago_acct(self):
-        df_chicago = self.df[self.df['Name'] == 'Chicago, IL']
-        df_chicago = df_chicago.astype({'Accounts':int})
-        chicago_acct = df_chicago['Accounts']
-        print(df_chicago)
-    # print(chicago_acct)
+        #find number of chicago accounts
+        chicago_acct = sum(self.chicago['Accounts'])
+        return chicago_acct
 
-get_comed_data.find_chicago_acct()
+    def total_acct(self):
+        #find total number of accounts in COMED
+        accts = sum(self.zip['Accounts'])
+        return accts
+
+    def chicago_perc(self):
+        #find the percentage of accounts that are in Chicago area
+        chicago_acct = self.find_chicago_acct()
+        accts = self.total_acct()
+        chicago_percentage = chicago_acct/accts
+        return chicago_percentage
+
+    def account_hourly(self):
+        #calculate the energy consumption for each account
+        total_acct = self.total_acct()
+        self.comed['COMED_MW'] = self.comed['COMED_MW'] / total_acct * 1000000
+        self.comed.rename(columns = {'COMED_MW':'COMED_W'}, inplace = True)
+        return self.comed
+
+
+df_chicago_hourly = get_comed_data().account_hourly()
+print(df_chicago_hourly.head())
