ec91633a7708fc14a1191b73a88c2c8b6835c196
wang1784 <57235185+wang1784@users.noreply.github.com>
2020-12-03 22:31:43 -0500

    get_solar_data
    extract data of solar generated power

---
+++ get_solar_data.py
@@ -0,0 +1,34 @@
+#import libraries needed
+from pvlib.pvsystem import PVSystem
+from pvlib.location import Location
+from pvlib.modelchain import ModelChain
+import pvlib
+import pandas as pd
+
+def get_solar_data():
+	### Set up parameters for PV model ###
+	#copied from examples for the following lines
+	sandia_modules = pvlib.pvsystem.retrieve_sam('SandiaMod')
+	module = sandia_modules['Canadian_Solar_CS5P_220M___2009_'] 
+	sapm_inverters = pvlib.pvsystem.retrieve_sam('cecinverter')
+	temperature_model_parameters = pvlib.temperature.TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_glass']
+	inverter = sapm_inverters['ABB__MICRO_0_25_I_OUTD_US_208__208V_']
+	system = PVSystem(module_parameters=module,
+										inverter_parameters=inverter,
+										temperature_model_parameters=temperature_model_parameters)
+										
+	#set up location and time ###
+	coordinates = [(41.881832, -87.623177, 'Chicago', 594, 'Etc/GMT+6')] #set up chicago as location
+	naive_times = pd.date_range(start='2015', end='2016', freq='1h') #set up time frame and frequency as 1 hour
+
+	### Get hourly solar power generation ###
+	for latitude, longitude, name, altitude, timezone in coordinates:
+			times = naive_times.tz_localize(timezone)
+			location = Location(latitude, longitude, name=name, altitude=altitude,
+													tz=timezone)
+			weather = location.get_clearsky(times)
+			mc = ModelChain(system, location,
+											orientation_strategy='south_at_latitude_tilt')
+			mc.run_model(weather)
+			
+	return mc.ac
