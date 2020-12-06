a0328161c04b3a15849d56aa02bbd4eb6c38dc9c
wang1784 <57235185+wang1784@users.noreply.github.com>
2020-12-06 18:20:17 -0500

    update env
    1. changed state space shape from tuple to list

--- solar_power_env.py
+++ solar_power_env.py
@@ -51,7 +51,7 @@ class solar_power_env():
         self._state = [p_init['solar_bin'], p_init['comed_bin'], 0] #initiate state
 
         #state space shape
-        self._state_space_shape = (len(self._solar_bin)-1, len(self._comed_bin)-1, len(self._battery_bin)-1)
+        self._state_space_shape = [len(self._solar_bin)-1, len(self._comed_bin)-1, len(self._battery_bin)-1]
 
     #assemble state information
     def extract_from_table(self, item): #finds p_solar, p_comed, OR bin_solar, bin_comed
